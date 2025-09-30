278b63b866b655fc97aff2c5d37517a23bdd340d
Finrei28 <161308914+Finrei28@users.noreply.github.com>
2025-09-30 17:00:36 +1300

    edited prisma schema

---
+++ prisma/migrations/20250927050751_add_cancel_to_membership/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Membership" ADD COLUMN     "cancel" BOOLEAN NOT NULL DEFAULT false;
---
+++ prisma/migrations/20250930032150_added_restaurant_status/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "RestaurantStatus" (
+    "id" TEXT NOT NULL,
+    "dineInAvailability" BOOLEAN NOT NULL,
+
+    CONSTRAINT "RestaurantStatus_pkey" PRIMARY KEY ("id")
+);
