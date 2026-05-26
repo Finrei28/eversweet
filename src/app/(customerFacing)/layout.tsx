c18e3626fc7409cb98d6429817941631f4983190
Finrei28 <161308914+Finrei28@users.noreply.github.com>
2026-05-26 16:08:45 +1200

    moved notification to layout so each page can get it

--- src/app/(customerFacing)/layout.tsx
+++ src/app/(customerFacing)/layout.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+import NotificationModal from "../_components/_homeComponents/notification";
 import { useLanguage } from "../components/language";
 import { Navbar, NavbarLink } from "../components/navbar";
 
@@ -7,6 +9,7 @@ export default function CustomerFacingLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   const { language } = useLanguage();
+  const [notificationModalOpen, setNotificationModalOpen] = useState(true);
   return (
     <>
       <Navbar>
@@ -25,6 +28,15 @@ export default function CustomerFacingLayout({
       </Navbar>
 
       <div>{children}</div>
+      <NotificationModal
+        open={notificationModalOpen}
+        onClose={() => setNotificationModalOpen(false)}
+        title={language === "en" ? "Announcement" : "通知"}
+      >
+        {language === "en"
+          ? "We are closed today on the 26/05/2026. Sorry for the inconvenience! We will be back to normal business hours tomorrow. Thank you for your understanding!"
+          : "我们将于2026年5月26日（今天）休息一天。对于由此带来的不便，我们深表歉意！我们将于明天恢复正常营业时间。感谢您的理解！"}
+      </NotificationModal>
     </>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+// @ts-ignore: side-effect import for global CSS without type declarations
 import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
