5a5e654e5b1f0057b3f8e4078a5cb41d85602b44
Finrei28 <161308914+Finrei28@users.noreply.github.com>
2026-08-05 16:58:12 +1200

    edited getDaysOff function

--- src/server/api/routers/store.ts
+++ src/server/api/routers/store.ts
@@ -1,8 +1,17 @@
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { DateTime } from "luxon";
 
 export const storeRouter = createTRPCRouter({
   getDaysOff: publicProcedure.query(async ({ ctx }) => {
-    const daysOff = await ctx.db.daysOff.findMany({ select: { date: true } });
+    const today = DateTime.now()
+      .setZone("Pacific/Auckland")
+      .startOf("day")
+      .toJSDate();
+    const daysOff = await ctx.db.daysOff.findMany({
+      select: { date: true },
+      where: { date: { gte: today } },
+      orderBy: { date: "asc" },
+    });
     const dates = daysOff.map((day) => day.date);
     return dates;
   }),
